309ab54dce9e2a1ce707bf6ab25e09ac3b04415f
cal <caelan.barr@gmail.com>
2016-04-14 15:20:38 -0700

    I wrote the code for the lab where you can click on a name and it will show their page with their information.

--- flask-app.py
+++ flask-app.py
@@ -1,3 +1,5 @@
+from os.path import dirname, join as join_path
+from os import chdir
 from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
@@ -10,7 +12,7 @@ def view_hello():
 
 @app.route('/demo-1')
 def view_demo_1():
-    return render_template('demo-1.html', name='Justin')
+    return render_template('demo-1.html', name='bob')
 
 @app.route('/demo-2/<name>')
 def view_demo_2(name):
@@ -33,7 +35,7 @@ class Student:
 
 def get_data():
     students = []
-    with open('students.csv') as fd:
+    with open(join_path(dirname(__file__), 'students.csv')) as fd:
         for line in fd.read().splitlines():
             name, username, majors, advisor = line.split('\t')
             last_name, first_name = name.split(', ')
@@ -42,11 +44,26 @@ def get_data():
 
 @app.route('/directory')
 def view_directory():
-    return 'FIXME'
+    stu = get_data()
+    return render_template('directory.html', students = stu)
 
 @app.route('/directory/<username>')
 def view_student(username):
-    return 'FIXME'
+    my_list = get_data()
+    for student in my_list:
+        if student.username == username:
+            student = student
+            number = my_list.index(student)
+            if my_list.index(student) == 0:
+                previous_student = my_list[-1]
+                next_student = my_list[number + 1]
+            elif my_list.index(student) == len(my_list) - 1:
+                next_student = my_list[0]
+                previous_student = my_list[number - 1]
+            else:
+                next_student = my_list[number + 1]
+                previous_student = my_list[number - 1]
+            return render_template('student.html', student = student, prev_student = previous_student, next_student = next_student)
 
 # DON'T TOUCH THE CODE BELOW THIS LINE
 
@@ -54,6 +71,11 @@ def view_student(username):
 def view_css(file):
     return send_from_directory('css', file)
 
+@app.route('/images/<file>')
+def view_images(file):
+    return send_from_directory('images', file)
+
 if __name__ == '__main__':
     #print(get_data())
+    chdir(dirname(__file__))
     app.run(debug=True)
